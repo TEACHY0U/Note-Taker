94559b37f31850f4f6a21eca9cbd8db12836e41c
TeachY0u <marianoteach@rocketmail.com>
2021-08-24 15:53:29 -0700

    using express and adding routes

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const exp = require("constants");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -6,3 +5,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require('./routes/routes')(app);
