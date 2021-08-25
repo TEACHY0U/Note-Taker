cf0d3c39b19ef7a53cc66eee08cffbcd763ce332
TeachY0u <marianoteach@rocketmail.com>
2021-08-24 19:36:35 -0700

    adding to routes and fixing server

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,12 @@ module.exports = function(app){
     
     // Route to go to notes page
     app.get("/notes", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+        res.sendFile(path.join(__dirname, "./public/notes.html"));
+    });
+
+    app.get("/", function(req, res){
+        res.sendFile(path.join(__dirname, './public/index.html'));
     });
 }
+
+
